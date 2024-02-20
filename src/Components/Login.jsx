675228e2f8a85491e8ef184b6aefd77358577727
Mahesh Gowda <maheshgowda3101@gmai.com>
2024-02-20 22:49:33 +0530

    updates

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,89 +1,101 @@
-import React, { useState , useEffect} from 'react';
-import { TextField, Button, Typography, Container} from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import './Styles/ExpenseTracker.css';
+import React, { useState, useEffect } from "react";
+import { TextField, Button, Typography, Container } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./Styles/ExpenseTracker.css";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    localStorage.setItem('authenticatedUser',JSON.stringify('') );
-  },[])
+  useEffect(() => {
+    localStorage.setItem("authenticatedUser", JSON.stringify(""));
+  }, []);
 
   const handleLogin = () => {
-  
-    if (username === '' || password === '') {
-      setError('Please enter both username and password.');
+    if (username === "" || password === "") {
+      setError("Please enter both username and password.");
       return;
     }
 
-    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const users = JSON.parse(localStorage.getItem("users")) || [];
 
     const authenticatedUser = users.find((user) => user.username === username);
 
     if (authenticatedUser && authenticatedUser.password === password) {
-      localStorage.setItem('authenticatedUser',JSON.stringify(username) );
-      navigate('/ExpenseTracker');
+      localStorage.setItem("authenticatedUser", JSON.stringify(username));
+      navigate("/ExpenseTracker");
     } else {
-      setError('Invalid username or password.');
+      setError("Invalid username or password.");
     }
   };
 
   return (
-  <div id='login-container'>
-    <div id='title-form'>
-      <div style={{marginLeft:'15px',marginTop:'50px',marginRight:'25px'}}>
-    <h1 style={{fontSize:'400px',marginTop:'0.05px'}}>₹</h1>
-    </div>
-    <div style={{fontSize:'70px',marginTop:'55px',flexWrap:'nowrap'}}>
-    <h1>Expense<br/>Tracker</h1>
-    
-    </div>
-    </div>
-    <div id='login-form'>
-      <br /><br /><br /><br /><br />
-    <Container  maxWidth="xs">
-    <Typography variant="h4" align="center" gutterBottom>
-        User
-      </Typography> <br /> 
-      <Typography variant="h4" align="center" gutterBottom>
-        Sign-In
-      </Typography>
-      {error && <Typography color="error">{error}</Typography>}
-      <TextField
-        label="Username"
-        variant="outlined"
-        fullWidth
-        margin="normal"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        className="custom-textfield"
-      />
-      <TextField
-        label="Password"
-        type="password"
-        variant="outlined"
-        fullWidth
-        margin="normal"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        className="custom-textfield"
-      /> <br /> <br />
-      <Button variant="contained" color="primary" fullWidth onClick={handleLogin}>
-        Login
-      </Button>
-      <Typography variant="body2" align="center" style={{ marginTop: '16px' }}>
-        Don't have an account?{' '}
-        <Link to="/Register" className='link-custom'>
-          Register
-        </Link>
-      </Typography>
-    </Container>
-    </div>
+    <div id="login-container">
+      <div id="title-form">
+        <div
+          style={{ marginLeft: "15px", marginTop: "50px", marginRight: "25px" }}
+        >
+          <h1 style={{ fontSize: "400px", marginTop: "0.05px" }}>₹</h1>
+        </div>
+        <div
+          style={{ fontSize: "70px", marginTop: "55px", flexWrap: "nowrap" }}
+        >
+          <h1>
+            Expense
+            <br />
+            Tracker
+          </h1>
+        </div>
+      </div>
+      <div id="login-form">
+        <Container maxWidth="xs">
+          <Typography variant="h4" align="center" gutterBottom>
+            Sign-In
+          </Typography>
+          {error && <Typography color="error">{error}</Typography>}
+          <TextField
+            label="Username"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="custom-textfield"
+          />
+          <TextField
+            label="Password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="custom-textfield"
+          />{" "}
+          <br /> <br />
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={handleLogin}
+          >
+            Login
+          </Button>
+          <Typography
+            variant="body2"
+            align="center"
+            style={{ marginTop: "16px" }}
+          >
+            Don't have an account?{" "}
+            <Link to="/Register" className="link-custom">
+              Register
+            </Link>
+          </Typography>
+        </Container>
+      </div>
     </div>
   );
 };
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,104 +1,116 @@
-import React, { useState } from 'react';
-import { TextField, Button, Typography, Container} from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import './Styles/ExpenseTracker.css';
+import React, { useState } from "react";
+import { TextField, Button, Typography, Container } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./Styles/ExpenseTracker.css";
 
 const Register = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const handleRegister = () => {
-    
-    if (username === '' || password === '' || confirmPassword === '') {
-      setError('Please fill in all fields.');
+    if (username === "" || password === "" || confirmPassword === "") {
+      setError("Please fill in all fields.");
       return;
     }
 
     if (password !== confirmPassword) {
-      setError('Passwords do not match.');
+      setError("Passwords do not match.");
       return;
     }
 
-    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const users = JSON.parse(localStorage.getItem("users")) || [];
 
     const userExists = users.some((user) => user.username === username);
 
     if (userExists) {
-      setError('Username already exists. Please choose another.');
+      setError("Username already exists. Please choose another.");
       return;
     }
 
     const newUser = { username, password };
     users.push(newUser);
-    localStorage.setItem('users', JSON.stringify(users));
-    navigate('/');
+    localStorage.setItem("users", JSON.stringify(users));
+    navigate("/");
   };
 
   return (
-    <div id='login-container'>
-      <div id='title-form'>
-      <div style={{marginLeft:'15px',marginTop:'50px',marginRight:'25px'}}>
-    <h1 style={{fontSize:'400px',marginTop:'0.05px'}}>₹</h1>
-    </div>
-    <div style={{fontSize:'50px',marginTop:'100px'}}>
-    <h1>Expense</h1>
-    <h1>Tracker</h1>
-    </div>
-    </div>
-    <div id='login-form'>
-      <br /><br /><br /><br /><br />
-    <Container  maxWidth="xs">
-    <Typography variant="h4" align="center" gutterBottom>
-        User
-      </Typography> <br /> 
-      <Typography variant="h4" align="center" gutterBottom>
-        Sign-Up
-      </Typography>
-      {error && <Typography color="error">{error}</Typography>}
-      <TextField
-        label="Username"
-        variant="outlined"
-        fullWidth
-        margin="normal"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        className="custom-textfield"
-      />
-      <TextField
-        label="Password"
-        type="password"
-        variant="outlined"
-        fullWidth
-        margin="normal"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        className="custom-textfield"
-      />
-      <TextField
-        label="Confirm Password"
-        type="password"
-        variant="outlined"
-        fullWidth
-        margin="normal"
-        value={confirmPassword}
-        onChange={(e) => setConfirmPassword(e.target.value)}
-        className="custom-textfield"
-      /> <br /> <br />
-      <Button variant="contained" color="primary" fullWidth onClick={handleRegister}>
-        Register
-      </Button>
-      <Typography variant="body2" align="center" style={{ marginTop: '16px' }}>
-        Already have an account?{' '}
-        <Link to='/' className='link-custom'>
-          Login
-        </Link>
-      </Typography>
-    </Container>
-    </div>
+    <div id="login-container">
+      <div id="title-form">
+        <div
+          style={{ marginLeft: "15px", marginTop: "50px", marginRight: "25px" }}
+        >
+          <h1 style={{ fontSize: "400px", marginTop: "0.05px" }}>₹</h1>
+        </div>
+        <div
+          style={{ fontSize: "70px", marginTop: "55px", flexWrap: "nowrap" }}
+        >
+          <h1>
+            Expense
+            <br />
+            Tracker
+          </h1>
+        </div>
+      </div>
+      <div id="login-form">
+        <Container maxWidth="xs">
+          <Typography variant="h4" align="center" gutterBottom>
+            Sign-Up
+          </Typography>
+          {error && <Typography color="error">{error}</Typography>}
+          <TextField
+            label="Username"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="custom-textfield"
+          />
+          <TextField
+            label="Password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="custom-textfield"
+          />
+          <TextField
+            label="Confirm Password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className="custom-textfield"
+          />{" "}
+          <br /> <br />
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={handleRegister}
+          >
+            Register
+          </Button>
+          <Typography
+            variant="body2"
+            align="center"
+            style={{ marginTop: "16px" }}
+          >
+            Already have an account?{" "}
+            <Link to="/" className="link-custom">
+              Login
+            </Link>
+          </Typography>
+        </Container>
+      </div>
     </div>
   );
 };
